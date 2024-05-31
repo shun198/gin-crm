7c5c122dd40e8f68ef86ab107b6d8655bf7e1537
s.hirose <shunichi_hirose@pj100.biz>
2024-05-31 14:09:10 +0900

    👍 delete session

--- application/main.go
+++ application/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"database/sql"
 	"log"
-	"os"
 
 	// "net/http"
 	"time"
 
 	"github.com/gin-contrib/cors"
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/postgres"
 	"github.com/gin-gonic/gin"
 	database "github.com/shun198/gin-crm/config"
 	_ "github.com/shun198/gin-crm/docs"
@@ -46,20 +42,6 @@ func main() {
 			log.Printf("データベースの接続の切断に失敗しました:%v", err)
 		}
 	}()
-	db, err := sql.Open("postgres", "postgresql://postgres:postgres@db:5432/postgres")
-	if err != nil {
-		log.Printf("データベースの接続の切断に失敗しました:%v", err)
-	}
-	store, _ := postgres.NewStore(db, []byte(os.Getenv("SECRET_KEY")))
-	r.Use(sessions.Sessions("session", store))
-	// https://pkg.go.dev/github.com/marktohark/gin-csrf#section-readme
-	// r.Use(csrf.Middleware(csrf.Options{
-	// 	Secret: "csrf_token",
-	// 	ErrorFunc: func(c *gin.Context) {
-	// 		c.String(http.StatusForbidden, "無効なCSRFトークンです")
-	// 		c.Abort()
-	// 	},
-	// }))
 	routes.GetCommonRoutes(r)
 	routes.GetUserRoutes(r, client)
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
